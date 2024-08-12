57e67c77bfaa096c54660fb816236b633cd25b37
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-08-12 12:40:33 +0530

    navBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import logo from "../src/assets/logo.png";
+import { NAVIGATION_LINKS } from "./constants";
+import { FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa6";
 
 const NavBar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const toggleMoBileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenu);
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
   const handleLinkClick = (e, href) => {
     e.preventDefault();
@@ -21,13 +25,54 @@ const NavBar = () => {
   };
   return (
     <div>
-      <nav className="fixed left-0 right-0 tio-4 z-50">
+      <nav className="fixed left-0 right-0 top-4 z-50">
         {/* desktop */}
-        <div className="mx-auto hidden max-w-2xl items-center justify-center rounded-lg border border-stone-50/30 bg-black/20 py-3 backdrop-blur-lg lg:flex">
+        <div className="mx-auto hidden max-w-2xl items-center justify-center rounded-lg border border-stone-50/30 bg-black/20 py-3 backdrop-blur-lg lg:flex lg:justify-evenly">
           <div className="flex items-center justify-between gap-6">
-            <a href="#">
-              <img src="" alt="" />
-            </a>
+            <div>
+              <a href="#">
+                <img src={logo} width={150} alt="logo" />
+              </a>
+            </div>
+          </div>
+          <div>
+            <ul className="flex items-center gap-4">
+              {NAVIGATION_LINKS.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a
+                      className="text-sm hover:text-yellow-400"
+                      href={item.href}
+                      onClick={(e) => handleLinkClick(e, item.href)}
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+        {/* mobileMenu */}
+        <div className="rounded-lg backdrop-blur-md lg:hidden">
+          <div className="flex items-center justify-between">
+            <div>
+              <a href="#">
+                <img src={logo} alt="logo" width={90} className="m-2" />
+              </a>
+            </div>
+            <div className="flex items-center">
+              <button
+                className="focus:outline-none lg:hidden"
+                onClick={toggleMoBileMenu}
+              >
+                {isMobileMenuOpen ? (
+                  <FaTimes className="m-2 h-6 w-5" />
+                ) : (
+                  <FaBars className="m-2 h-6 w-5" />
+                )}
+              </button>
+            </div>
           </div>
         </div>
       </nav>
